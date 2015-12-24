b5b3f1c51e0061272932d9311371b17c8e0abe45
RICK STEELE <patrickjsteele@gmail.com>
2015-12-24 12:41:07 -0800

    removing some debug logs

--- dribbble_conversion.js
+++ dribbble_conversion.js
@@ -3,12 +3,9 @@ var allShots = document.getElementsByClassName("dribbble-shot");
 
 // for every shot
 for (var i = 0; i < allShots.length; i++){
-  // var views = allShots[i].getElementsByClassName('views');
 
   // we can safely grab these based on 0 indeces
   // because there's only 1 views per shot, and 1 span per view
-  
-  // var span = views[0].getElementsByTagName('span')[0].textContent;
   var viewCount = allShots[i].getElementsByClassName('views')[0].getElementsByTagName('span')[0].textContent;
 
   // fav count is 2nd a tag (first is the clickable heart)
