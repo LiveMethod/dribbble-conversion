64ccf29e214bffb7e32d1975dd586891bd2280d8
RICK STEELE <patrickjsteele@gmail.com>
2015-12-24 16:56:20 -0800

    now works on single pages, removed icons

--- dribbble_conversion.js
+++ dribbble_conversion.js
@@ -9,38 +9,72 @@ function updateShotStats(){
     // we can safely grab these based on 0 indeces
     // because there's only 1 views per shot, and 1 span per view
     var viewCount = allShots[i].getElementsByClassName('views')[0].getElementsByTagName('span')[0].textContent;
+    // strip commas
+    viewCount = parseFloat(viewCount.replace(',',''));
 
     // fav count is 2nd a tag (first is the clickable heart)
     var likeCount = allShots[i].getElementsByClassName('fav')[0].getElementsByTagName('a')[1].textContent;
-
-    viewCount = parseFloat(viewCount.replace(',',''));
+    // strip commas
     likeCount = parseFloat(likeCount.replace(',',''));
 
     // calculate percentage of viewers who liked, as string "(xx%)"
     var likePercent = Math.round((likeCount/viewCount) * 100) + "%";
-
 
     // the group (contains views, comments, likes, and soon pcnt)
     var group = allShots[i].getElementsByClassName('group')[0];
 
     // if a percentage LI exists,
     if (group.getElementsByClassName('percent').length > 0){
+      // use existing tag
       pcnt = group.getElementsByClassName('percent')[0];
       pcnt.textContent = likePercent;
     } else {
-      // create new LI to add to the group
+      // create new li to add to the group
       var pcnt = document.createElement('li');
       // add class
       pcnt.className += ' percent';
       // set its text
       pcnt.textContent = likePercent;
 
-      // group.appendChild(pcnt);
       // group LIs are reverse floated, so first = last
       group.insertBefore(pcnt, group.firstChild);
       // append it
     }
   } // for every shot
+}
+
+function updateSinglePage(){
+  var mainShot = document.getElementsByClassName('main-shot')[0];
+  
+  var viewCount = mainShot.getElementsByClassName('meta-views')[0].textContent;
+  // split string
+  viewCount = viewCount.split(' ')[0];
+  // strip commas
+  viewCount = parseFloat(viewCount.replace(',',''));
+
+  // regex the numbers out of the "x likes" a string
+  var likeCount = mainShot.getElementsByClassName('fav-number')[0].getElementsByTagName('a')[0].textContent.match(/\d+/)[0];
+  // strip commas
+  likeCount = parseFloat(likeCount.replace(',',''));
+  
+  var likePercent = "(" + Math.round((likeCount/viewCount) * 100) + "%)";
+
+  // if a percentage LI exists,
+  if (mainShot.getElementsByClassName('percent').length > 0){
+    // use existing tag
+    pcnt = mainShot.getElementsByClassName('percent')[0];
+    pcnt.textContent = likePercent;
+  } else {
+    var pcnt = document.createElement('span');
+    // add class
+    pcnt.className += ' percent';
+    // set its text
+    pcnt.textContent = likePercent;
+
+    mainShot.getElementsByClassName('fav-number')[0].getElementsByTagName('a')[0].appendChild(pcnt);
+
+    console.log(likeCount);
+  }
 }
 
 function getAllShots(){
@@ -55,13 +89,17 @@ function checkShots(){
     lastShotCount = allShots.length;
     updateShotStats();
   }
+
+  if (document.getElementsByClassName('main-shot').length > 0){
+    updateSinglePage();
+  }
 }
 
 // init
 checkShots();
 
-// 4x/s 
-setInterval(
+// 4x/s
+var lazyLoadChecker = setInterval(
   function(){
     checkShots();
   }, 
