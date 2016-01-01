4e7168d943316186b347e93ad332534c74cec2e8
RICK STEELE <patrickjsteele@gmail.com>
2016-01-01 17:56:58 -0500

    fixed bug on single pages with >999 likes, cleaned up some stuff

--- src/dribbble_conversion.js
+++ src/dribbble_conversion.js
@@ -1,8 +1,10 @@
+// store the number of shots when last checked
 var lastShotCount = 0;
+// will become an array of shot DOM nodes
 var allShots = undefined;
 
-function updateShotStats(){
-
+// loop for pages with many shots
+function updateMultiShotPage(){
   // for every shot
   for (var i = 0; i < allShots.length; i++){
 
@@ -10,12 +12,12 @@ function updateShotStats(){
     // because there's only 1 views per shot, and 1 span per view
     var viewCount = allShots[i].getElementsByClassName('views')[0].getElementsByTagName('span')[0].textContent;
     // strip commas
-    viewCount = parseFloat(viewCount.replace(',',''));
+    viewCount = cleanDigits(viewCount);
 
     // fav count is 2nd a tag (first is the clickable heart)
     var likeCount = allShots[i].getElementsByClassName('fav')[0].getElementsByTagName('a')[1].textContent;
     // strip commas
-    likeCount = parseFloat(likeCount.replace(',',''));
+    likeCount = cleanDigits(likeCount);
 
     // calculate percentage of viewers who liked, as string "(xx%)"
     var likePercent = Math.round((likeCount/viewCount) * 100) + "%";
@@ -43,56 +45,69 @@ function updateShotStats(){
   } // for every shot
 }
 
+// individual shot pages
 function updateSinglePage(){
+  // get the main shot
   var mainShot = document.getElementsByClassName('main-shot')[0];
   
   var viewCount = mainShot.getElementsByClassName('meta-views')[0].textContent;
   // split string
   viewCount = viewCount.split(' ')[0];
   // strip commas
-  viewCount = parseFloat(viewCount.replace(',',''));
+  viewCount = cleanDigits(viewCount);
 
-  // regex the numbers out of the "x likes" a string
-  var likeCount = mainShot.getElementsByClassName('fav-number')[0].getElementsByTagName('a')[0].textContent.match(/\d+/)[0];
+
+  // regex the numbers out of the "x likes" string
+  var likeCount = mainShot.getElementsByClassName('fav-number')[0].getElementsByTagName('a')[0].textContent;
   // strip commas
-  likeCount = parseFloat(likeCount.replace(',',''));
+  likeCount = cleanDigits(likeCount);
   
+  // // calculate percentage of viewers who liked, as string "(xx%)"
   var likePercent = "(" + Math.round((likeCount/viewCount) * 100) + "%)";
 
-  // if a percentage LI exists,
+  // if a percentage li already exists,
   if (mainShot.getElementsByClassName('percent').length > 0){
     // use existing tag
     pcnt = mainShot.getElementsByClassName('percent')[0];
     pcnt.textContent = likePercent;
   } else {
+    // make a span
     var pcnt = document.createElement('span');
     // add class
     pcnt.className += ' percent';
     // set its text
     pcnt.textContent = likePercent;
-
+    // append to doc
     mainShot.getElementsByClassName('fav-number')[0].getElementsByTagName('a')[0].appendChild(pcnt);
-
-    console.log(likeCount);
   }
 }
 
+// get all shots on a multiple shot page
 function getAllShots(){
   shots = document.getElementsByClassName("dribbble-shot");
   return shots;
 }
 
+// check for new shots
 function checkShots(){
   allShots = getAllShots();
 
-  if (allShots.length > lastShotCount){
-    lastShotCount = allShots.length;
-    updateShotStats();
-  }
-
+  // the 'main-shot' class is distinct to the single page,
+  // so this is a cheap way to determine which page type we're on
   if (document.getElementsByClassName('main-shot').length > 0){
     updateSinglePage();
   }
+
+  // if there are new shots (due to ajax loads), update.
+  if (allShots.length > lastShotCount){
+    lastShotCount = allShots.length;
+    updateMultiShotPage();
+  }
+}
+
+// clean up numbers with commas
+function cleanDigits(data){
+  return parseFloat(data.replace(',',''));
 }
 
 // init
@@ -102,6 +117,7 @@ checkShots();
 var lazyLoadChecker = setInterval(
   function(){
     checkShots();
-  }, 
+  },
+  // this number could be tweaked...
   250
 );
